5a4a0499722e73c4b70e4fb94bacb70f273e7e1d
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-18 21:15:41 +0300

    Add files via upload

---
+++ YARAcleanner.py
@@ -0,0 +1,49 @@
+import os
+import yara
+import re
+
+# Directory containing YARA rules
+yara_directory = 'YARA'
+
+def comment_out_errors(file_path, error_message):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    modified_lines = []
+
+    # Extract the line number from the error message using regular expression
+    error_match = re.search(r'\((\d+)\)', error_message)
+    error_line = int(error_match.group(1) if error_match else -1)
+
+    for line_number, line in enumerate(lines, start=1):
+        if line_number == error_line:
+            modified_lines.append(f'// {line.strip()}')
+            print(f'Processed Line {line_number}: {line.strip()}')
+        else:
+            modified_lines.append(line)
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.writelines(modified_lines)
+
+# Process all ".yara" files in the specified directory
+while True:
+    errors_found = False
+
+    for root, _, files in os.walk(yara_directory):
+        for file in files:
+            if file.endswith('.yara'):
+                file_path = os.path.join(root, file)
+
+                # Use YARA Python library to validate the rule file
+                try:
+                    rules = yara.compile(filepath=file_path)
+                except yara.SyntaxError as e:
+                    error_message = str(e)
+                    comment_out_errors(file_path, error_message)
+                    print(f'Processed: {file_path} - Error message: {error_message}')
+                    errors_found = True
+
+    if not errors_found:
+        break
+
+print('YARA rules processed successfully.')
