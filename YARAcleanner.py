91b0443157cda6f4b97ab804c5c2a50ecb122c3d
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-19 15:13:10 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -10,17 +10,25 @@ def comment_out_errors(file_path, error_message):
         lines = f.readlines()
 
     modified_lines = []
-
-    # Extract the line number from the error message using regular expression
-    error_match = re.search(r'\((\d+)\)', error_message)
-    error_line = int(error_match.group(1) if error_match else -1)
+    error_line = -1
 
     for line_number, line in enumerate(lines, start=1):
-        if line_number == error_line:
-            modified_lines.append(f'// {line.strip()}')
-            print(f'Processed Line {line_number}: {line.strip()}')
-        else:
+        if error_line == -1:
+            if re.search(r'\((\d+)\)', error_message):
+                error_match = re.search(r'\((\d+)\)', error_message)
+                error_line = int(error_match.group(1))
+
+        if error_line == -1:
             modified_lines.append(line)
+        else:
+            if line_number == error_line:
+                modified_lines.append(f'// {line.strip()}')
+                print(f'Processed Line {line_number}: {line.strip()}')
+            elif line.strip() == "}":
+                error_line = -1
+                modified_lines.append(line)
+            else:
+                modified_lines.append(f'// {line.strip()}')
 
     with open(file_path, 'w', encoding='utf-8') as f:
         f.writelines(modified_lines)
