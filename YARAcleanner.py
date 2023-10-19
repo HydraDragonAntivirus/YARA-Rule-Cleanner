c53dd759b3d2a726b1723953d9d84b8e3c5a393e
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-19 11:24:20 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -1,31 +1,26 @@
 import os
 import yara
+import re
 
 # Directory containing YARA rules
 yara_directory = 'YARA'
 
-def comment_out_private_or_rule(file_path):
+def comment_out_errors(file_path, error_message):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     modified_lines = []
-
     inside_rule = False
 
     for line in lines:
-        if (
-            line.strip().startswith("private rule")
-            or line.strip().startswith("rule")
-            or line.strip().startswith("}private rule")
-            or line.strip().startswith("}rule")
-        ):
-            inside_rule = True
         if inside_rule:
             modified_lines.append(f'// {line.strip()}')
         else:
             modified_lines.append(line)
 
-        if inside_rule and line.strip() == "}":
+        if line.strip().startswith("rule"):
+            inside_rule = True
+        if inside_rule and line.strip().endswith("}"):
             inside_rule = False
 
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -43,9 +38,13 @@ for root, _, files in os.walk(yara_directory):
             try:
                 rules = yara.compile(filepath=file_path)
             except yara.SyntaxError as e:
-                comment_out_private_or_rule(file_path)
-                print(f'Processed: {file_path} - Rule commented out due to error')
-                errors_found = True
+                error_message = str(e)
+                if "compilation error" in error_message:
+                    comment_out_errors(file_path, error_message)
+                    print(f'Processed: {file_path} - Rule commented out due to error')
+                    errors_found = True
 
 if errors_found:
     print('YARA rules contain errors and have been processed with comments.')
+else:
+    print('YARA rules processed successfully.')
