85cf44515074f23bb56e16832cb1e261cb30a634
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-20 17:56:30 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -6,7 +6,7 @@ import re
 yara_directory = 'YARA'
 
 def comment_out_rule(file_path, rule_name):
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
         lines = f.readlines()
 
     modified_lines = []
@@ -19,7 +19,10 @@ def comment_out_rule(file_path, rule_name):
         if in_rule_block:
             modified_lines.append(f'// {line.strip()}')
         else:
-            modified_lines.append(line)
+            if is_utf8(line):
+                modified_lines.append(f'// {line.strip()}')
+            else:
+                modified_lines.append(line)
 
         if line.strip() == "}":
             in_rule_block = False
@@ -27,31 +30,41 @@ def comment_out_rule(file_path, rule_name):
     with open(file_path, 'w', encoding='utf-8') as f:
         f.writelines(modified_lines)
 
-# Process all ".yar" files in the specified directory
-while True:
-    errors_found = False
+def is_utf8(line):
+    try:
+        line.encode('utf-8')
+        return True
+    except UnicodeEncodeError:
+        return False
 
-    for root, _, files in os.walk(yara_directory):
-        for file in files:
-            if file.endswith('.yar'):
-                file_path = os.path.join(root, file)
+def process_yara_rules():
+    while True:
+        errors_found = False
 
-                try:
-                    rules = yara.compile(filepath=file_path)
-                except yara.SyntaxError as e:
-                    error_message = str(e)
-                    error_match = re.search(r'\((\d+)\)', error_message)
-                    error_line = int(error_match.group(1) if error_match else -1)
-                    
-                    if error_line != -1:
-                        # Extract the rule name
-                        rule_name = re.search(r'rule (\S+)', lines[error_line - 1]).group(1)
+        for root, _, files in os.walk(yara_directory):
+            for file in files:
+                if file.endswith('.yar'):
+                    file_path = os.path.join(root, file)
+
+                    try:
+                        rules = yara.compile(filepath=file_path)
+                    except yara.SyntaxError as e:
+                        error_message = str(e)
+                        error_match = re.search(r'\((\d+)\)', error_message)
+                        error_line = int(error_match.group(1) if error_match else -1)
                         
-                        comment_out_rule(file_path, rule_name)
-                        print(f'Processed: {file_path} - Error message: {error_message}')
-                        errors_found = True
+                        if error_line != -1:
+                            with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                                lines = f.readlines()
+                            rule_name = re.search(r'rule (\S+)', lines[error_line - 1]).group(1)
+                            comment_out_rule(file_path, rule_name)
+                            print(f'Processed: {file_path} - Error message: {error_message}')
+                            errors_found = True
 
-    if not errors_found:
-        break
+        if not errors_found:
+            break
 
-print('YARA rules processed successfully.')
+    print('YARA rules processed successfully.')
+
+if __name__ == '__main__':
+    process_yara_rules()
