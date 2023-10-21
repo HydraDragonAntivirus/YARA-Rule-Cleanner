c88f167bdd0496446db92a3f49d2850b155b4ed5
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-21 11:28:46 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -1,63 +1,54 @@
 import os
 import yara
 import re
-import codecs
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 # Directory containing YARA rules
 yara_directory = 'YARA'
 
+def compile_yara_rules(file_path):
+    try:
+        return yara.compile(filepath=file_path)
+    except yara.SyntaxError as e:
+        return None, str(e)
+
 def comment_out_errors(file_path, error_message):
-    with codecs.open(file_path, 'r', encoding='ISO8859-1') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
     modified_lines = []
-
-    # Extract the line number from the error message using regular expression
-    error_match = re.search(r'\((\d+)\)', error_message)
-    error_line = int(error_match.group(1) if error_match else -1)
+    error_line = int(re.search(r'\((\d+)\)', error_message).group(1))
 
     for line_number, line in enumerate(lines, start=1):
         if line_number == error_line:
             modified_lines.append(f'// {line.strip()}')
-            print(f'Processed Line {line_number}: {line.strip()}')
         else:
             modified_lines.append(line)
 
-    with codecs.open(file_path, 'w', encoding='utf-8') as f:
+    with open(file_path, 'w', encoding='utf-8') as f:
         f.writelines(modified_lines)
 
-def scan_and_process_yara_file(file_path):
-    try:
-        rules = yara.compile(filepath=file_path)
-    except yara.SyntaxError as e:
-        error_message = str(e)
+def process_yara_file(file_path):
+    rules, error_message = compile_yara_rules(file_path)
+    if rules is None:
         comment_out_errors(file_path, error_message)
         print(f'Processed: {file_path} - Error message: {error_message}')
-        return True  # Indicate an error was found
+        return True
     return False
 
-# Process all ".yara" files in the specified directory using multiprocessing
-while True:
-    errors_found = False
+if __name__ == "__main__":
+    while True:
+        errors_found = False
 
-    with ProcessPoolExecutor() as executor:
-        yara_files = []
+        with ThreadPoolExecutor() as executor:
+            for root, _, files in os.walk(yara_directory):
+                for file in files:
+                    if file.endswith('.yara'):
+                        file_path = os.path.join(root, file)
+                        if process_yara_file(file_path):
+                            errors_found = True
 
-        for root, _, files in os.walk(yara_directory):
-            for file in files:
-                if file.endswith('.yara'):
-                    file_path = os.path.join(root, file)
-                    yara_files.append(file_path)
+        if not errors_found:
+            break
 
-        # Submit file processing tasks and check for errors
-        error_results = list(executor.map(scan_and_process_yara_file, yara_files))
-
-        # Check if any errors were found
-        if any(error_results):
-            errors_found = True
-
-    if not errors_found:
-        break
-
-print('YARA rules processed successfully.')
+    print('YARA rules processed successfully.')
