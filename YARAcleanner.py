5b272ccfe517f8b74244908a21b0ed0e473fb6c1
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-21 11:38:50 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -1,54 +1,54 @@
 import os
 import yara
 import re
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 # Directory containing YARA rules
 yara_directory = 'YARA'
-
-def compile_yara_rules(file_path):
-    try:
-        return yara.compile(filepath=file_path)
-    except yara.SyntaxError as e:
-        return None, str(e)
 
 def comment_out_errors(file_path, error_message):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    modified_lines = []
     error_line = int(re.search(r'\((\d+)\)', error_message).group(1))
 
+    modified_lines = []
     for line_number, line in enumerate(lines, start=1):
         if line_number == error_line:
             modified_lines.append(f'// {line.strip()}')
+            print(f'Processed Line {line_number}: {line.strip()}')
         else:
             modified_lines.append(line)
 
     with open(file_path, 'w', encoding='utf-8') as f:
         f.writelines(modified_lines)
 
-def process_yara_file(file_path):
-    rules, error_message = compile_yara_rules(file_path)
-    if rules is None:
+def process_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+    except UnicodeDecodeError:
+        print(f'UnicodeDecodeError for file: {file_path}. Skipping this file.')
+        return False
+
+    try:
+        rules = yara.compile(filepath=file_path)
+    except yara.SyntaxError as e:
+        error_message = str(e)
         comment_out_errors(file_path, error_message)
         print(f'Processed: {file_path} - Error message: {error_message}')
         return True
     return False
 
-if __name__ == "__main__":
-    while True:
-        errors_found = False
+# Process all ".yara" files in the specified directory using parallel processing
+errors_found = False
+with ProcessPoolExecutor() as executor:
+    file_paths = [os.path.join(root, file) for root, _, files in os.walk(yara_directory) for file in files if file.endswith('.yara')]
+    error_results = list(executor.map(process_file, file_paths))
+    if any(error_results):
+        errors_found = True
 
-        with ThreadPoolExecutor() as executor:
-            for root, _, files in os.walk(yara_directory):
-                for file in files:
-                    if file.endswith('.yar'):
-                        file_path = os.path.join(root, file)
-                        if process_yara_file(file_path):
-                            errors_found = True
-
-        if not errors_found:
-            break
-
+if errors_found:
+    print('YARA rules processed with errors.')
+else:
     print('YARA rules processed successfully.')
