4cee266b96cad2554eced742cd4bd17eea24f31d
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-21 12:19:32 +0300

    Update YARAcleanner.py

--- YARAcleanner.py
+++ YARAcleanner.py
@@ -1,7 +1,7 @@
 import os
 import yara
 import re
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
 # Directory containing YARA rules
 yara_directory = 'YARA'
@@ -10,37 +10,32 @@ def comment_out_errors(file_path, error_message):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    error_line_match = re.search(r'\((\d+)\)', error_message)
-    error_line = int(error_line_match.group(1)) if error_line_match else -1
-
     modified_lines = []
-    comment_started = False
+
+    # Extract the line number from the error message using regular expression
+    error_match = re.search(r'\((\d+)\)', error_message)
+    error_line = int(error_match.group(1) if error_match else -1)
+
+    flag_above = False
 
     for line_number, line in enumerate(lines, start=1):
-        if comment_started:
-            if line.strip().startswith('}'):
-                comment_started = False
+        if line_number == error_line:
+            # Check if the line already has '//'
+            if '//' in line:
+                flag_above = True
             modified_lines.append(f'// {line.strip()}')
-        elif line_number == error_line:
-            if not line.strip().startswith('//'):
-                modified_lines.append(f'// {line.strip()}')
-                comment_started = True
-            else:
-                modified_lines.append(line)
+            print(f'Processed Line {line_number}: {line.strip()}')
+        elif flag_above:
+            modified_lines.append(f'// {line.strip()}')
+            print(f'Processed Line {line_number}: {line.strip()}')
+            flag_above = False
         else:
             modified_lines.append(line)
 
     with open(file_path, 'w', encoding='utf-8') as f:
         f.writelines(modified_lines)
 
-def process_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-    except UnicodeDecodeError:
-        print(f'UnicodeDecodeError for file: {file_path}. Skipping this file.')
-        return False
-
+def process_yara_file(file_path):
     try:
         rules = yara.compile(filepath=file_path)
     except yara.SyntaxError as e:
@@ -50,16 +45,18 @@ def process_file(file_path):
         return True
     return False
 
-# Process all ".yar" files in the specified directory using parallel processing
-while True:
-    errors_found = False
-    with ProcessPoolExecutor() as executor:
-        file_paths = [os.path.join(root, file) for root, _, files in os.walk(yara_directory) for file in files if file.endswith('.yar')]
-        error_results = list(executor.map(process_file, file_paths))
-        if any(error_results):
-            errors_found = True
+if __name__ == '__main__':
+    with ThreadPoolExecutor() as executor:
+        while True:
+            errors_found = False
 
-    if not errors_found:
-        break
+            for root, _, files in os.walk(yara_directory):
+                yara_files = [os.path.join(root, file) for file in files if file.endswith('.yara')]
+                results = list(executor.map(process_yara_file, yara_files))
+                if any(results):
+                    errors_found = True
 
-print('YARA rules processed successfully.')
+            if not errors_found:
+                break
+
+    print('YARA rules processed successfully.')
